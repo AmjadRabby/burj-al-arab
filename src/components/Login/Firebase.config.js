967a01620ce35b4a6a5ad8081dec84e7cffa9b11
AmjadRabby <glamarrabby@gmail.com>
2020-09-12 01:01:08 +0600

    first commit

---
+++ src/components/Login/Firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBEtA6-K03UMnVjA16iBU3c1k7VGvGI09w",
+    authDomain: "burj-al-arab-46402.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-46402.firebaseio.com",
+    projectId: "burj-al-arab-46402",
+    storageBucket: "burj-al-arab-46402.appspot.com",
+    messagingSenderId: "692996273136",
+    appId: "1:692996273136:web:b344c587cd36a0c907a335"
+  };
